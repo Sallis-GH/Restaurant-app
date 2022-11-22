e254a1bc226bbd7a10293d9ccba369e29b186889
Sallis <Salamonerorvik@gmail.com>
2022-11-22 12:19:42 +0100

    fixed design issues

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,15 +24,15 @@ const Header = () => {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <img src={logo} alt="" srcset="" className='header-logo-mobile' />
+        <img src={logo} alt="logo" className='header-logo-mobile' />
         <div className="collapse navbar-collapse pt-2" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0 align-items-center">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
             <li className="nav-item">
               <Link to='/'>
-              <img src={logo} alt="" srcset="" className='header-logo' />
+              <img src={logo} alt="logo" className='header-logo' />
               </Link>
             </li>
-            <li className="nav-item ms-3">
+            <li className="nav-item">
               <Link className="nav-link" href="#">Link</Link>
             </li>
             <li className="nav-item dropdown">
