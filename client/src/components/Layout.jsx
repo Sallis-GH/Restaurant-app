fc58bf5bfbc87856de7059a6060d3053ea9f3399
Sallis <Salamonerorvik@gmail.com>
2022-11-22 11:12:24 +0100

    inital header design implemented

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
+import Header from './Header';
 
 const Layout = () => {
   return (
     <>
-    <h1>Header</h1>
+    <Header />
     <Outlet />
-    <h1>Footer</h1>
     </>
   )
 }
