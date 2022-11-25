ec2ce028cfe9921aeccf37a184b96db61cf1718b
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2022-11-25 11:49:22 +0100

    busines-addMenuPage

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,14 @@ import Layout from './components/Layout';
 import About from './pages/About';
 import NoPage from './pages/NoPage';
 import Menu from './pages/Menu';
+import AddMenu from './pages/AddMenu';
 
 function App() {
   return (
     <Routes>
       <Route index element={<Home />} />
+
+      <Route path="/business-login/addmenu" element={<AddMenu />} />
 
     <Route path="/" element={<Layout />}>
       <Route path="about" element={<About />} />
