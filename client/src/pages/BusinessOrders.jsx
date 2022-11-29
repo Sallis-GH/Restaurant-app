ffbbee27a90c90f995bf3cb232865ff56147f23d
Andrea-Agosta <andrea.agosta85@gmail.com>
2022-11-29 09:00:33 +0100

    update function complete Order and delete order in DB

--- client/src/pages/BusinessOrders.jsx
+++ client/src/pages/BusinessOrders.jsx
@@ -7,8 +7,10 @@ import '../__style__/businessOrder.css';
 const BusinessOrders = () => {
   const [orders, setOrders] = useState([]);
   const [userInfo, setUserInfo] = useState([]);
+  const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
+    console.log('use effect called');
     axios.get('http://localhost:8080/api/orders')
       .then(async response => {
         setUserInfo(response.data);
@@ -17,11 +19,15 @@ const BusinessOrders = () => {
         )
       })
       .catch(error => console.log(error))
-  }, []);
+  }, [refresh]);
 
-  const completeOrder = () => {
-
+  const completeOrder = event => {
+    axios.delete(`http://localhost:8080/api/orders/${userInfo[event.target.id].id}`)
+      .catch(error => console.log(error))
+    setTimeout(() => setRefresh(!refresh), 500);
   };
+
+  console.log(refresh);
 
   // return (
   //   <div className='container mt-4'>
@@ -57,39 +63,41 @@ const BusinessOrders = () => {
   return (
     <div className="container">
       <div className="row">
-        {userInfo?.map((order, index) => (
-          <div className="col-6">
-            <div className="card mb-3 shadow-sm card-width" key={index} >
-              <div className="row g-0">
-                <div className="col-md-2 image">
-                  <img src="https://static.vecteezy.com/system/resources/previews/002/554/006/large_2x/catering-food-service-fast-delivery-logistic-line-style-icon-free-vector.jpg" className="img-fluid rounded-start" alt="..." />
-                </div>
-                <div className="col-md-9">
-                  <div className="card-body pe-0">
-                    <div className="row" >
-                      <div className="col-4">
-                        <h5 className="card-title">NEW ORDER</h5>
-                        {orders[index].map((dish, index) => <span key={index} > {dish} <br /> </span>)}
+        {
+          userInfo?.map((order, index) => (
+            <div className="col-6" key={index}>
+              <div className="card mb-3 shadow-sm card-width" >
+                <div className="row g-0">
+                  <div className="col-md-2 image">
+                    <img src="https://static.vecteezy.com/system/resources/previews/002/554/006/large_2x/catering-food-service-fast-delivery-logistic-line-style-icon-free-vector.jpg" className="img-fluid rounded-start" alt="order_img" />
+                  </div>
+                  <div className="col-md-9">
+                    <div className="card-body pe-0">
+                      <div className="row" >
+                        <div className="col-4">
+                          <h5 className="card-title">NEW ORDER</h5>
+                          {orders[index].map((dish, index) => <span key={index} > {dish} <br /> </span>)}
+                        </div>
+                        <div className="col-4">
+                          <h5 className="card-title">TIME</h5>
+                          {order.date} <span> <br /> {order.time} </span>
+                        </div>
+                        <div className="col-4">
+                          <h5 className="card-title">USER</h5>
+                          {order.email} <span> <br /> {order.phone} <br /> </span> {order.address}
+                        </div>
                       </div>
-                      <div className="col-4">
-                        <h5 className="card-title">TIME</h5>
-                        {order.date} <span> <br /> {order.time} </span>
+                      <div className="d-flex justify-content-end mt-2">
+                        <button id={index} className="btn btn-outline-secondary me-3" onClick={(e) => completeOrder(e)} >Decline</button>
+                        <button id={index} className="btn btn-outline-success" onClick={(e) => completeOrder(e)} >Complete</button>
                       </div>
-                      <div className="col-4">
-                        <h5 className="card-title">USER</h5>
-                        {order.email} <span> <br /> {order.phone} <br /> </span> {order.address}
-                      </div>
-                    </div>
-                    <div className="d-flex justify-content-end mt-2">
-                      <button className="btn btn-outline-secondary me-3" onClick={completeOrder} >Decline</button>
-                      <button className="btn btn-outline-success" onClick={completeOrder} >Complete</button>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))
+        }
       </div>
     </div>
   )
