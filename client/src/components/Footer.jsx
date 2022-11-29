6208206d129bf6578485a98f1dae2340a2b14f07
Andrea-Agosta <andrea.agosta85@gmail.com>
2022-11-29 10:48:33 +0100

    update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { Instagram, Facebook, Whatsapp } from 'react-bootstrap-icons';
 const Footer = () => {
   return (
     <>
-      <div className="footer lightgray px-4">
+      <div className="footer lightgray px-4 ">
         <div className="border-bottom border-secondary p-3 pb-lg-0">
           <div className="row">
             <div className="col-6">
@@ -29,9 +29,8 @@ const Footer = () => {
             </div>
           </div>
           <div className="col d-flex flex-column flex-md-row justify-content-end mb-2">
-            <Link to={'#'} className="mt-2 fs-6 text-decoration-none lightgray hover text-md-end">Menu</Link>
-            <Link to={'#'} className="mt-2 fs-6 text-decoration-none lightgray hover mx-md-5 text-md-end">About</Link>
-            <Link to={'#'} className="mt-2 fs-6 text-decoration-none lightgray hover text-md-end">Contact Us</Link>
+            <Link to={'/menu'} className="mt-2 fs-6 text-decoration-none lightgray hover text-md-end">Menu</Link>
+            <Link to={'/about'} className="mt-2 fs-6 text-decoration-none lightgray hover mx-md-5 text-md-end">About</Link>
           </div>
         </div>
       </div>
